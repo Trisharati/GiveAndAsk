e5c644d592f2f03830eefef65c99b58260c9b4ce
parnika mukherjee <parnika.mukherjee13@gmail.com>
2024-02-13 17:28:40 +0530

    profile-header-footer

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light " 
-      style={{backgroundColor: "#e3f2fd"}}>
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <div className='mobile-container d-flex'>
   <a className="navbar-brand" href="#"><img src="img/evo_connect.png" className="logo-img"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -19,19 +19,19 @@ const Navbar = () => {
       <li className="nav-item active">
         {/* <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a> */}
       </li>
-      <li className="nav-item">
+      {/* <li className="nav-item">
         <a className="nav-link" href="#" onClick={()=>navigate('/giveablelist')}>Give</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#" onClick={()=>navigate('/asklist')}>Ask</a>
-      </li>
+      </li> */}
     </ul>
     <ul className="navbar-nav ml-auto">
     {/* <i class="fas fa-user"></i>  */}
       <li className="nav-item dropdown">       
        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         {/* <p></p> */}
-        {localStorage.getItem('user_name')}<i class="fas fa-user"></i>
+        {localStorage.getItem('user_name')} <i class="fas fa-user"></i>
   {/* <i class="fas fa-caret-down ml-1"></i> */}
           </a>
        
@@ -45,6 +45,7 @@ const Navbar = () => {
         </div>
       </li>
     </ul>
+  </div>
   </div>
 </nav>
 
